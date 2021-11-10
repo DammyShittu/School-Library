b45a90ec895f2717e082d94b83a2a21d921b9ef8
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-11-10 14:18:21 +0100

    Create Classroom Class and Add Association Between Student And Classroom Class

--- student.rb
+++ student.rb
@@ -3,6 +3,8 @@
 require_relative 'person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission = true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -10,6 +12,11 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
 
